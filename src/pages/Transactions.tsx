46e3b3b6c5e2ab305217a643d97fd47546c06c04
Wageesha95 <imwageesha@gmail.com>
2026-04-05 00:39:37 +0530

    Add summary totals to transaction group view

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -306,6 +306,16 @@ export default function Transactions() {
                       </tr>
                     ))}
                   </tbody>
+                  <tfoot>
+                    <tr className="portfolio-total">
+                      <td colSpan={2}>Total ({txns.length})</td>
+                      <td className="text-right mono">{txns.reduce((s, t) => s + (t.type === 'BUY' || t.type === 'RIGHTS' || t.type === 'SCRIP_DIVIDEND' ? t.count : -t.count), 0)}</td>
+                      <td className="text-right mono"></td>
+                      <td className="text-right mono">{txns.reduce((s, t) => s + t.commission, 0).toFixed(2)}</td>
+                      <td className="text-right mono">{txns.reduce((s, t) => s + (t.count * t.price + t.commission), 0).toFixed(2)}</td>
+                      <td></td>
+                    </tr>
+                  </tfoot>
                 </table>
               </div>
             </div>
